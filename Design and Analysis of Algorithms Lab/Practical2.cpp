282315b4f66fb48dace78ae59169fb692c1fdb6d
Pranjal Rai <thepranjalrai@pranjal-msi.rai-residence.com>
2021-02-01 22:26:14 +0530

    DAA P2 Documented

--- Design and Analysis of Algorithms Lab/Practical2.cpp
+++ Design and Analysis of Algorithms Lab/Practical2.cpp
@@ -4,11 +4,14 @@
 #include <math.h>
 #include <iomanip>
 #include <stdlib.h>
+#include <algorithm>
+#include <climits>
+
+#define cell_size 25
+#define filler ' '
 
 using namespace std;
 using namespace chrono;
-
-int recursion_depth = 0;
 
 void swap(int *a, int *b)
 {
@@ -29,14 +32,27 @@ void fill_elements(int* array, int size)	//Function to randomly initialize sampl
 	}
 }
 
+void print_number(long num, string terminator = "")  //To print numbers formatted by commas
+{
+    string s = std::to_string(num);
+    int n = s.length() - 3;
+    while (n > 0) {
+        s.insert(n, ",");
+        n -= 3;
+    }
+    cout << setw(cell_size-5) << s << terminator;
+}
+
 class arrays
 {
 public:
-    int* findMOM(int** addresses, int size)
+    
+    //Address Oriented implementation (My implementation)
+    int* findMOM(int** addresses, int size) //Returns the memory address of Median of Medians of given array
     {
-        const int split = 5;
-
-        if (size <= split)
+        const int split = 5;                //Subset size per iteration i.e. array[45] = 9 subarrays
+                                            
+        if (size <= split)                  //Insertion sort and return median
         {
             for (int i = 0; i < size; i++)
                 for (int j = i+1; j < size; j++)
@@ -46,146 +62,261 @@ public:
                         addresses[i] = addresses[j];
                         addresses[j] = dummy;
                     }
-            
-            //for(int i=0; i<size; i++) cout << setw(4) << *addresses[i];
-            //cout << "\t";
-            //print(">>GroundSort", addresses, size);
             return (addresses[size/2]);
         }
 
-        int* shortlist[size/split];
-        int* subset[split];
+        int* shortlist[size/split];         //to store median of each subset
+        int* subset[split];                 //to store one subset at a time
         for(int i = 0; i < size; i+=split)
         {
             int subset_size = (size-i >= split? split : size-i);
-            
             //2833 ahmd howrah
-
-            //cout << "\n" << i << "\tsubset_size = " << subset_size <<endl;
-            for(int j = 0; j < split; j++)
+            for(int j = 0; j < split; j++)  //Extract from original array into subset
                 subset[j] = addresses[i+j];
 
-            //print("Subset", subset, subset_size);
-            shortlist[i/split] = findMOM(subset, subset_size);
-            //cout << "shortlist_idx : " << i/split << " = " << shortlist[i/split] << endl;
+            shortlist[i/split] = findMOM(subset, subset_size);  //Put Medians of subset into shortlist
         }
 
-        //for(int i = 0; i < size/split; i++) cout << setw(4) << shortlist[i];
-        //print("Shortlist", shortlist, size/split);
-
-
-        return findMOM(shortlist, size/split);
-        //for(int i=0; i<size; i++) cout << setw(4) << array[i];
+        return findMOM(shortlist, size/split);  //Recur to find median of the (shortlisted) medians
     }
 
     void quick_sort(int* arr, int low, int high)    //Entire original array, low:high are slice indices wrt original array
     {
         if(low < high)
         {
-            recursion_depth++;
-            char filler = '\t';
-            
-            //cout <<"\n\n";
-            //cout << setfill(filler) << setw(recursion_depth) << "Recursion Depth = " << recursion_depth << endl;
-            //cout << setfill(filler) << setw(recursion_depth) << "Low : High = " << low << " : " << high << endl;
-            
-            //int** addresses = (int**) malloc (high-low+1 * sizeof(int*));
-            int* addresses[high-low+1];
+            int* addresses[high-low+1]; //Map the partition(array) elements with their respective addresses
+            for(int i=0; i< high-low+1; i++) addresses[i] = &arr[low+i];
 
-            for(int i=0; i< high-low+1; i++)
-            {
-                addresses[i] = &arr[low+i];
-                //cout << setfill(filler) << setw(recursion_depth) << "" << addresses[i] << "\t" << *addresses[i] << "\t" << arr[i] << endl;
-            }
+            int pivot_index = findMOM(addresses, high-low+1) - arr; //Pivot index is w.r.t. original array
 
-            int partition_index = findMOM(addresses, high-low+1) - arr; //Partition index is w.r.t. original array
-            
-            //cout << setfill(filler) << setw(recursion_depth) << "" << endl << partition_index + arr << "\tarr[" << partition_index << "] = " << arr[partition_index] << "\n";
-
-            int dummy = arr[partition_index];
-            arr[partition_index] = arr[high];
-            arr[high] = dummy;
+            swap(arr[pivot_index], arr[high]);  //Swap pivot with the last element in partition(array)
 
             int pivot = arr[high];
             int i = low - 1; // Index of smaller element  
 
-            //print("............\nPass..", array_1, size_1);
+            for (int j = low; j <= high - 1; j++)               //Throw elements around pivot
+                if (arr[j] < pivot) swap(&arr[++i], &arr[j]);  
 
-            for (int j = low; j <= high - 1; j++)  
+            swap(&arr[i + 1], &arr[high]);                      //Place pivot
+            
+            int m (i + 1);                                      //Pivot index
+            quick_sort(arr, low, m - 1);                        //Recur left of pivot
+            quick_sort(arr, m + 1, high);                       //Recur right of pivot
+        }
+    }
+    
+    //Basic Implementation  (My implementation)
+    int partition(int* arr, int low, int high)  
+    {  
+        int pivot = arr[high]; // pivot  
+        int i = (low - 1); // Index of smaller element  
+    
+        for (int j = low; j <= high - 1; j++)   
+            if (arr[j] < pivot)  
+                swap(&arr[++i], &arr[j]);  
+        
+        swap(&arr[i + 1], &arr[high]);  
+        return (i + 1);  
+    }  
+    
+    void basic_quick_sort(int* arr, int low, int high)  
+    {  
+        if (low < high)  
+        {  
+            int m = partition(arr, low, high);  
+    
+            // Separately sort elements before  
+            // partition and after partition  
+            basic_quick_sort(arr, low, m - 1);  
+            basic_quick_sort(arr, m + 1, high);  
+        }  
+    }  
+
+    //Search for MoM based implementation (Taken from geeksforgeeks.com)
+    int partition(int arr[], int l, int r, int x)       // It searches for x in array, and partitions the array around x.
+    {
+        // Search for x in array and move it to end
+        int i;
+        for (i=l; i<r; i++)
+            if (arr[i] == x)
+            break;
+        swap(&arr[i], &arr[r]);
+    
+        // Standard partition algorithm
+        i = l;
+        for (int j = l; j <= r - 1; j++)
+        {
+            if (arr[j] <= x)
             {
-                //cout << "arr[" << j << "] = " << arr[j] << " | Pivot = " << pivot;
-                if (arr[j] < pivot)  
-                {  
-                    i++; 
-                    swap(&arr[i], &arr[j]);
-                    //cout << "\tSwapped..";
-                }
-                //print("\n > ", array_1, size_1);
-            }  
-            swap(&arr[i + 1], &arr[high]);  
-            int m (i + 1);
-
-            //if(addresses) free(addresses);
-
-            quick_sort(arr, low, m - 1);  
-            quick_sort(arr, m + 1, high); 
-            recursion_depth--;
+                swap(&arr[i], &arr[j]);
+                i++;
+            }
+        }
+        swap(&arr[i], &arr[r]);
+        return i;
+    }
+    
+    int findMedian(int arr[], int n)                    // A simple function to find median of arr[] For array size of 5
+    {
+        sort(arr, arr+n); // Sort the array
+        return arr[n/2]; // Return middle element
+    }
+    
+    int kthSmallest(int arr[], int l, int r, int k)     // Returns k'th smallest element in array in worst case linear time.
+    {
+        // If k is smaller than number of elements in array
+        if (k > 0 && k <= r - l + 1)
+        {
+            int n = r-l+1; // Number of elements in arr[l..r]
+    
+            // Divide arr[] in groups of size 5, calculate median of every group and store it in median[] array.
+            int i, median[(n+4)/5]; // There will be floor((n+4)/5) groups;
+            for (i=0; i<n/5; i++)
+                median[i] = findMedian(arr+l+i*5, 5);
+            if (i*5 < n) //For last group with less than 5 elements
+            {
+                median[i] = findMedian(arr+l+i*5, n%5);
+                i++;
+            }
+    
+            // Find median of all medians using recursive call if more than 1 median
+            int medOfMed = (i == 1)? median[i-1]:
+                                    kthSmallest(median, 0, i-1, i/2);
+    
+            // Partition the array around a random element and get position of pivot element in sorted array
+            int pos = partition(arr, l, r, medOfMed);
+    
+            // If position is same as k
+            if (pos-l == k-1)
+                return arr[pos];
+            if (pos-l > k-1) // If position is more, recur for left
+                return kthSmallest(arr, l, pos-1, k);
+    
+            // Else recur for right subarray
+            return kthSmallest(arr, pos+1, r, k-pos+l-1);
+        }
+    
+        // If k is more than number of elements in array
+        return INT_MAX;
+    }
+    
+    void mom_quick_sort(int arr[], int l, int h)
+    {
+        if (l < h)
+        {
+            // Find size of current subarray
+            int n = h-l+1;
+            // Find median of arr[]
+            int med = kthSmallest(arr, l, h, n/2);
+    
+            // Partition the array around median
+            int p = partition(arr, l, h, med);
+    
+            // Recur for left and right of partition
+            mom_quick_sort(arr, l, p - 1);
+            mom_quick_sort(arr, p + 1, h);
         }
     }
 
-    void print(string title, int** addresses, int size)
+    //Utility Functions
+    void print(string title, int** addresses, int size)         //To print array by given address list
     {
         cout << '\n' << title << '\n';
         
         for(int i=0; i<size; i++)
-        {
             cout << *addresses[i] << "  ";
-        }
 
         cout << '\n';
     }
-
-    void _print(string title, int *arr, int arr_size)
+    
+    void _print(string title, int *arr, int arr_size)           //To print given array
     {
         cout << '\n' << title << '\n';
         
         for(int i=0; i<arr_size; i++)
-        {
             cout << arr[i] << "  ";
-        }
 
         cout << '\n';
     }
 };
 
-int main()
+void analyse(int* arr, int size, int iterations)
 {
+    cout << "\n\nElements : " << size << endl << endl;
+    cout << setfill(filler) << setw(cell_size-10) << "Iteration";
+    cout << setfill(filler) << setw(cell_size) << "My MoM + QS";
+    cout << setfill(filler) << setw(cell_size) << "Mom + QS of GFG";
+    cout << setfill(filler) << setw(cell_size) << "Basic QS";
+    cout << "\n";
+
     arrays array_manager;
-    
-    cout << "EXECUTING\n\n";
-    cout << "Enter Array Size : ";
-    int size;
-    cin >> size;
 
-    int* arr = (int*) malloc(size * sizeof(int));
-    int** addresses = (int**) malloc (size * sizeof(int*));
+    long a_times_sum = 0, b_times_sum = 0, c_times_sum = 0;
 
-    fill_elements(arr, size);
-    for(int i=0; i<size; i++)
+    for(int i = 0; i < iterations; i++)
     {
-        addresses[i] = &arr[i];
-        //cout << addresses[i] << "\t" << *addresses[i] << "\t" << arr[i] << endl;
+        fill_elements(arr, size);
+
+        int* copy1  = (int*) malloc(size * sizeof(int));    //Making copies for each methods
+        int* copy2  = (int*) malloc(size * sizeof(int));    //because some operations may be in-place
+        int* copy3  = (int*) malloc(size * sizeof(int));
+        copy(arr, arr+size, copy1);
+        copy(arr, arr+size, copy2);
+        copy(arr, arr+size, copy3);
+
+        cout << setw(cell_size-11) << i << "]";
+        //array_manager._print("Original Array : ", arr, size);
+
+        auto start = high_resolution_clock::now();					//Start timing
+            //array_manager._print("Copy1 Array : ", arr, size);
+            array_manager.quick_sort(copy1, 0, size-1);
+            //array_manager._print("Sorted : ", copy1, size);
+        auto stop = high_resolution_clock::now();					//Stop timing
+        auto duration_first = duration_cast<nanoseconds>(stop - start);
+
+        print_number(duration_first.count(), " (ns)");
+        a_times_sum += duration_first.count();
+
+        start = high_resolution_clock::now();					//Start timing
+            //array_manager._print("Copy2 Array : ", arr, size);
+            array_manager.mom_quick_sort(copy2, 0, size-1);
+            //array_manager._print("Sorted : ", copy2, size);
+        stop = high_resolution_clock::now();					//Stop timing
+        auto duration_second = duration_cast<nanoseconds>(stop - start);
+
+        print_number(duration_second.count(), " (ns)");
+        b_times_sum += duration_second.count();
+        
+        start = high_resolution_clock::now();					//Start timing
+            //array_manager._print("Copy2 Array : ", arr, size);
+            array_manager.basic_quick_sort(copy2, 0, size-1);
+            //array_manager._print("Sorted : ", copy2, size);
+        stop = high_resolution_clock::now();					//Stop timing
+        auto duration_third = duration_cast<nanoseconds>(stop - start);
+
+        print_number(duration_third.count(), " (ns)\n");
+        c_times_sum += duration_third.count();
     }
 
-    array_manager.print("Original Array : ", addresses, size);
-    array_manager.quick_sort(arr, 0, size-1);
-    array_manager.print("Sorted Array : ", addresses, size);
-    //int* momLoc = array_manager.findMOM(addresses, size);
-    //cout << "\n\nMomLOC : " << momLoc << "  =  " << *momLoc << endl;
+    cout << endl;
+    cout << setfill(filler) << setw(cell_size-10) << "Avg Time";
+    print_number(a_times_sum/iterations, " (ns)");
+    print_number(b_times_sum/iterations, " (ns)");
+    print_number(c_times_sum/iterations, " (ns)");
+}
 
-    //int momidx = array_manager.findMOM(arr, size, true);
-    //cout << "\n\n" << momidx << '\t' << array_manager.recursion_depth;
-    //for(int i=0; i<size; i++) cout << arr[i] << endl;
+int main()
+{    
+    cout << "EXECUTING";
+
+    const int size1 = 50000;
+    int* arr1 = (int*) malloc(size1 * sizeof(int));
+    
+    const int size2 = 100000;
+    int* arr2 = (int*) malloc(size2 * sizeof(int));
+
+    analyse(arr1, size1, 5);
+    analyse(arr2, size2, 5);
 
     return 0;
 }
