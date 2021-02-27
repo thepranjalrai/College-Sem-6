4d60f3f3dc67ae7a9316aa3f3c46da5b3e57d9a2
Pranjal Rai <thepranjalrai@pranjal-msi.rai-residence.com>
2021-02-27 20:30:36 +0530

    Worked on DAA P5

--- Design and Analysis of Algorithms Lab/Practical5.cpp
+++ Design and Analysis of Algorithms Lab/Practical5.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <utility>
+#include <set>
 
 #include <chrono>		// duration()
 #include <stdlib.h>     // srand(), rand()
@@ -10,16 +11,19 @@ using namespace std;
 
 #define edge pair< node*, int >
 
-#define MAX_NODES 10
+#define MAX_NODES 5
+#define MAX_EDGES 15
 #define MAX_EDGES_PER_NODE 3   //Counts outgoing edges only
 #define MAX_WEIGHT 10
+
+#define LOOP_THRESHOLD 500
 
 class node
 {
     unsigned id;                //Node id as integer starting from 1
     
     unsigned edge_count;
-    edge* edges;
+    edge edges[MAX_EDGES_PER_NODE];
 
     static unsigned node_count;
     static node* nodes;
@@ -30,13 +34,13 @@ public:
         id = ++node_count;
         edge_count = 0;
 
-        //cout << "\nCreated..\n";
+        //cout << "Created.. ";
         //cout << "Node ID : " << id << endl;
     }
 
     inline ~node()
     {
-        cout << "\n wth, Node : " << id << "was killed.";
+        cout << "\nwth, Node : " << id << " was killed.";
     }
 
     static void create_graph()
@@ -45,12 +49,18 @@ public:
         {
             node* ptr = new node;
             nodes[i] = *ptr;
+            //usleep(500000);
         }
 
+        join_nodes_randomly();
+
+        /*
         for(int i = 0; i < MAX_NODES; i++)
         {
             nodes[i].set_random_edges();
         }
+        */
+
         //cout << "\nNode count is : " << node_count << endl;
     }
 
@@ -85,6 +95,7 @@ public:
         }
     }
 
+/*
     void set_random_edges()
     {
         edge_count = rand() % MAX_EDGES_PER_NODE + 1;
@@ -114,7 +125,52 @@ public:
             k *= 3.14;
         }
     }
+*/
+    static void join_nodes_randomly()
+    {
+        //cout << "\nCreating Edges..\n";
+        int looper = 0;
 
+        unsigned k = 2;
+        for(int i = 0; i < MAX_EDGES; i++)
+        {
+            if(looper > LOOP_THRESHOLD)
+                return;
+
+            node* node_a = &nodes[(rand() * k*k + k/2 )% node_count];
+            node* node_b = &nodes[(rand() * k*k + k/2 )% node_count];
+            int weight = rand() % MAX_WEIGHT + 1;
+
+            //Check if new edge is valid
+            bool valid = true;
+
+            for(edge e : node_a->edges)
+                if(e.first == node_b || node_a->id == node_b->id)
+                    valid = false;
+
+            if(!(node_a->edge_count < MAX_EDGES_PER_NODE && node_b->edge_count < MAX_EDGES_PER_NODE))
+                valid = false;
+            
+            if(valid)
+            {                
+                node_a->edges[node_a->edge_count].first = node_b;
+                node_a->edges[node_a->edge_count++].second = weight;
+
+                node_b->edges[node_b->edge_count].first = node_a;
+                node_b->edges[node_b->edge_count++].second = weight;
+
+                //cout << i+1 << "] " << node_a->id << ' ' << node_b->id << ' ' << weight << endl;
+            }
+            else
+            {
+                i--;
+                looper++;
+            }
+            
+            k *= 3.14;
+            if(k > 69420) k = 6.66;
+        }
+    }
 };
 
 unsigned node::node_count = 0;
